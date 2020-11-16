6937d5c6317e4454b013e82ff52bf95b397b9a8a
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-16 19:53:08 +0100

    Hold loading components till data is fetched

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -4,6 +4,9 @@ import { gameDetailsURL, gameScreenshotURL } from "../api";
 //Action Creator
 
 export const loadDetail = (id) => async (dispatch) => {
+  dispatch({
+    type: "LOADING_DETAIL",
+  });
   //FETCH AXIOS
   const detailData = await axios.get(gameDetailsURL(id));
   const screenshotData = await axios.get(gameScreenshotURL(id));
