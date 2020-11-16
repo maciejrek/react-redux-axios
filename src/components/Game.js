21636043820f5ad66cf637206c70e877647abc5a
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-16 20:20:23 +0100

    Toggling components

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,6 +10,7 @@ import { Link } from "react-router-dom";
 const Game = ({ name, released, id, image }) => {
   const dispatch = useDispatch();
   const loadDetailHandler = () => {
+    document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
   };
   return (
@@ -29,6 +30,7 @@ const StyledGame = styled(motion.div)`
   text-align: center;
   border-radius: 2rem;
   cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
     height: 40vh;
