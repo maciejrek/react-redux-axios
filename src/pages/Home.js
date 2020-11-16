8d45eed3b2a53e43d97a1c95570094452c9dcf26
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-16 19:46:35 +0100

    Add React Router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,12 @@ import GameDetail from "../components/GameDetail";
 //Styling and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  //get the current location
+  const location = useLocation();
+  const pathId = location.pathname.split("/")[2];
   //dispatch sends data to state
   const dispatch = useDispatch();
   useEffect(() => {
@@ -19,7 +23,7 @@ const Home = () => {
   const { popular, newGames, upcoming } = useSelector((state) => state.games);
   return (
     <GameList>
-      <GameDetail />
+      {pathId && <GameDetail />}
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
